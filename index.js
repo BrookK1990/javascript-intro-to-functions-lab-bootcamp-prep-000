7bf45b44c4ac42942d5256e841f9fef1c9b64db9
bkalk08@yahoo.com <bkalk08@yahoo.com>
2018-07-14 17:17:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var uppercase = "HELLO!"
+var lowercase = 'hello!'
+
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+logShout('hello')
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+logWhisper('HELLO')
+
+function sayHiToGrandma(string) {
+  if var===lowercase
+  return "I can\'t hear you!"
+else {
+
+}  if  var===uppercase
+  return "YES INDEED!"
+  else
